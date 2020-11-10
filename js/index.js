02114d1cecd122d6e0fcf284a292e2ec40aff81c
DESKTOP-M262J75\1강의실 <mucie2159@gmail.com>
2020-11-10 18:53:07 +0900

    fix markup and css , add vanila-js and jQuery

--- js/index.js
+++ js/index.js
@@ -10,3 +10,44 @@ var timeAuto = setInterval(time, 60000);
 window.onload = function() {
     time();
 }
+
+
+
+function galleryChange(num1) {
+    num=num1;
+    var arrFrameList=document.querySelectorAll('.aladin-tv-wrap .main iframe');
+    document.querySelector('.aladin-tv-wrap .main iframe.on').removeAttribute('class');
+    arrFrameList[num1].setAttribute('class','on');
+    return false;
+}
+
+var num=0;
+function autoAddNum() {
+    num++;
+    if (num>=4) num=0;
+    var aTag = document.querySelectorAll('.map-image a');
+    aTag[num].click();
+}
+
+
+
+function bannerMenu(paging,num) {
+    console.log(paging);
+    var arr = paging.className.split('');
+    if(arr.indexOf('on')<0){
+        console.log(paging);
+        document.querySelector('.on').classList.remove('on');
+        paging.classList.add('on');
+    }
+    var arrDiv = document.querySelectorAll('.slide-banner-wrap > .paged');
+    var num =0;
+    for (var i = 0; i < arrDiv.length; i++) {
+        arrDiv[i].style.height=0;
+        arrDiv[i].style.overflow='hidden';
+    }
+    arrDiv[num].style.height='auto';
+    console.log(arrDiv[num])
+}
+
+
+
