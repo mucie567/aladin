e94b1dfa700887a56fc4b2826645cc9879b333b4
DESKTOP-M262J75\1강의실 <mucie2159@gmail.com>
2020-11-11 13:41:57 +0900

    add markup goods.html

---
+++ js/goods.js
@@ -0,0 +1,11 @@
+// let lorem=document.querySelector('#container > div.goods-book-wrap > div.author > div > div.author-content > div.author-profile > p')
+// let moreBtn= document.querySelector('#container > div.goods-book-wrap > div.author > div > div.author-content > div.author-profile > button.btn-more')
+// let text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda consequuntur pariatur itaque nobis velit nostrum laborum. Magni quaerat libero debitis dignissimos possimus facilis, dolor at, sequi aliquam voluptatem quae inventore.`
+
+// lorem.innerText = text.substr(0, 100) + '...'
+
+// function showMore() {
+//     lorem.innerText = text
+// }
+
+// moreBtn.addEventListener('click',showMore)
