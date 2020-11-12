e9dcdec942ec7c7a2145c502dd90de549cce4101
최경진pc\경진 <mucie567@naver.com>
2020-11-12 22:14:42 +0900

    good.html add css and js

--- js/goods.js
+++ js/goods.js
@@ -1,6 +1,6 @@
-// let lorem=document.querySelector('#container > div.goods-book-wrap > div.author > div > div.author-content > div.author-profile > p')
-// let moreBtn= document.querySelector('#container > div.goods-book-wrap > div.author > div > div.author-content > div.author-profile > button.btn-more')
-// let text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda consequuntur pariatur itaque nobis velit nostrum laborum. Magni quaerat libero debitis dignissimos possimus facilis, dolor at, sequi aliquam voluptatem quae inventore.`
+// let lorem=document.querySelector('#container > div.goods-book-wrap > div.goods-back > div.author > div > div.author-content > div.author-profile > p.author-intro')
+// let moreBtn= document.querySelector('#container > div.goods-book-wrap > div.goods-back > div.author > div > div.author-content > div.author-profile > button.btn-more')
+// let text = `한국 판타지소설의 대표 작가이다. 새롭게 선보이는 작품마다 한국뿐만 아니라 일본, 대만, 중국을 비롯한 아시아권에서 베스트셀러로 등극하면서 베스트셀러 작가로 입지를 확실히 굳히며 독자들의 사랑을 받고 있다. 1972년에 태어났다. 두 살 때부터 마산에서 자라난 마산 토박이로 경남대학교 국어국문학과를 졸업했다. 1993년부터 본격적으로 소설을 쓰기 시작, 1997년 가을 컴퓨터 통신 하이텔에 판타지 장편소설 『드래곤 라자』를 연재했다. 일만 삼천여 매에 달하는 방대한 분량으로 이용자들의 폭발적인 부흥의 전기를 마련했다.1년 후 내놓은 『퓨처워커』는 한층 심도 있는 주제와 새로운 구성으로 전작을 뛰어넘는 작품성을 인정받았다. 그 후 『폴라리스 랩소디』를 출간하여 완성된 작품 스타일을 보여주었는데, 이 작품은 기존의 반양장 형태의 서적 외에도 500부 한정으로 고급 양장본으로 제작되어 단숨에 매진될 정도로 많은 이의 관심을 불러 모았다. 장대한 구상 위에 집필하여 2003년 내놓은 대작 『눈물을 마시는 새』는 한국적 소재를 자연스럽게 녹여낸 판타지 대하 소설로 이영도 붐을 새롭게 했다.무한한 상상력, 깊이 있는 세계관과 뛰어난 작품성으로 한국의 대표적 환상 문학으로 자리잡은 『드래곤 라자』는 10년간 국내에서만 100만 부가 넘게 판매되었고, 일본에서 현재까지 40만 부, 대만에서 30만 부, 중국에서는 10만 부가 판매되는 등 아시아 각국에서 폭넓은 사랑을 받았다. 저서로는 소설집 『오버 더 호라이즌』, 장편소설 『퓨처워커』, 『폴라리스 랩소디』, 『눈물을 마시는 새』, 『피를 마시는 새』 『그림자 자국』, 『오버 더 초이스』 등이 있다.`
 
 // lorem.innerText = text.substr(0, 100) + '...'
 
@@ -9,3 +9,4 @@
 // }
 
 // moreBtn.addEventListener('click',showMore)
+
