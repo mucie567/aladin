803b66d0c5f7cb888f3d6136a82f0b3d2918e121
DESKTOP-M262J75\1강의실 <mucie2159@gmail.com>
2020-11-16 08:21:15 +0900

    add

--- js/goods.js
+++ js/goods.js
@@ -1,1 +1,25 @@
+window.addEventListener('load',afterLoad);
+
+// window.onload = afterLoad;
+function stringCountCheck(element) {
+   var el = document.querySelector(element);
+   var inp = el.querySelector('.inp-reply-1');
+   var count = el.querySelector('.count');
+   var limit = el.querySelector('.limit');
+   var limitNum = Number(limit.innerHTML);
+   inp.addEventListener('keyup',function() {
+       var val = this.value;
+       if (val.length > limitNum) {
+            this.value = val.substr(0,limitNum)   
+        }
+    count.innerHTML = this.value.length;
+   });
+}
+function afterLoad() {
+    //댓글 문자수량체크
+    stringCountCheck('.reply-wrap');
+}
+
+
+
 
