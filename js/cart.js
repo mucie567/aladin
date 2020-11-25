2ff35514667845a9c1127d6747842d88f3f912da
DESKTOP-M262J75\1강의실 <mucie2159@gmail.com>
2020-11-25 16:06:24 +0900

    add js

--- js/cart.js
+++ js/cart.js
@@ -15,3 +15,85 @@ function tabMenu(btn,num) {
     }
     arrDiv[num].style.display='block';
 }
+
+
+function utilFnc(element) {
+    var util1 = element.find('>li>a')
+    var util1Site = element.find('>div')
+  
+  
+  
+    var oldMenu = null;
+      util1.on('mouseover',function() {
+          if (oldMenu!=null) {
+              oldMenu.removeClass('on');
+          }
+         $(this).addClass('on'); 
+         util1Site.stop(true).slideDown(500);
+         oldMenu=$(this);
+      });
+      element.on('mouseleave',function() {
+        util1Site.stop(true).slideUp(100);
+  
+          if (oldMenu!=null) {
+              oldMenu.removeClass('on');
+          }
+      })
+  }
+  
+  function gnbFnc(element) {
+      var gnb1 = element.find('>.all-menu>a')
+      var gnbMenu = element.find('>div')
+  
+      var oldMenu = null;
+      gnb1.on('mouseover',function() {
+          if (oldMenu!=null) {
+              oldMenu.removeClass('on');
+          }
+         $(this).addClass('on'); 
+         gnbMenu.stop(true).slideDown(500);
+         oldMenu=$(this);
+      });
+      element.on('mouseleave',function() {
+          gnbMenu.stop(true).slideUp(100);
+  
+          if (oldMenu!=null) {
+              oldMenu.removeClass('on');
+          }
+      })
+  }
+  
+  function subMenu(element) {
+      var sub1 = element.find('>li>a')
+      var subMenu = element.find('> div')
+  
+      var oldMenu = null;
+      sub1.on('mouseover',function() {
+          if (oldMenu!=null) {
+              oldMenu.removeClass('on');
+          }
+         $(this).addClass('on'); 
+         subMenu.stop(true).slideDown(500);
+         oldMenu=$(this);
+      });
+      element.on('mouseleave',function() {
+          subMenu.stop(true).slideUp(100);
+  
+          if (oldMenu!=null) {
+              oldMenu.removeClass('on');
+          }
+      })
+  }
+  
+
+
+
+
+
+
+$(function() {
+    utilFnc($('.siemap'))
+    gnbFnc($('.gnb-menu'))
+    subMenu($('.gnb'))
+    pcGnbMenuFnc();
+  })
